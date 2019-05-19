fa643d3df92036e1678ec83c48fd7f8296d7fe08
StefaPP <stefancina@live.com>
2019-05-19 20:33:53 +0200

    Added port to env variables

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 
 const PORT = 4000;
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log('Express server listening on port ' + PORT);
 });
