154d25d621ad6b380a58c2dfceecb51669028b15
StefaPP <stefancina@live.com>
2019-05-22 20:29:10 +0200

    Heroku fix attempt #4

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log('Express server listening on port ' + process.env.PORT);
+app.listen(PORT, () => {
+    console.log('Express server listening on port ' + PORT);
 });
